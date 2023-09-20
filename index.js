8e1381bda4159d6c6ecb832202ade075dc32c14f
jTech <kietpawpan@gmail.com>
2023-09-20 09:33:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ const convertToGrayScales = (context, width, height) => {
     return grayScales;
 };
 
-const MAXIMUM_WIDTH = 62;
-const MAXIMUM_HEIGHT = 62;
+const MAXIMUM_WIDTH = 70;
+const MAXIMUM_HEIGHT = 70;
 
 const clampDimensions = (width, height) => {
     const rectifiedWidth = Math.floor(getFontRatio() * width);
